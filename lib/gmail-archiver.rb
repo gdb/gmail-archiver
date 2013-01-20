8a251d110c5556de25d65416fba9ae1eda2b7c5e
Greg Brockman <gdb@gregbrockman.com>
2013-01-20 14:17:02 -0800

    Remove stripe-context dependency

--- lib/gmail-archiver.rb
+++ lib/gmail-archiver.rb
@@ -1,35 +1,53 @@
+require 'logger'
 require 'yaml'
+require 'tempfile'
 
 require 'rubygems'
-require 'stripe-context'
 require 'highline/import'
 
-#############################
+$log = Logger.new(STDERR)
+$log.level = Logger::INFO
 
-module StripeContext
-  module TopLevel
-    include StripeContext::Log::Loggable
+def assert(statement, msg=nil, hard=false)
+  return if statement
 
-    def self.assert(statement, msg=nil, hard=false)
-      return if statement
-
-      msg = msg ? "Assertion Failure: #{msg}" : "Assertion Failure"
-      formatted = "#{msg}\n  #{caller.join("\n  ")}"
-      log_error(formatted)
-      raise msg if hard
-    end
-  end
+  msg = msg ? "Assertion Failure: #{msg}" : "Assertion Failure"
+  formatted = "#{msg}\n  #{caller.join("\n  ")}"
+  $log.error(formatted)
+  raise msg if hard
 end
-
-def assert(*args)
-  StripeContext::TopLevel.assert(*args)
-end
-
-StripeContext::Log::Loggable.init('default_level' => 'WARN')
-
-############################
 
 module GmailArchiver
+  module Third
+    # From http://api.rubyonrails.org/classes/File.html
+    def self.atomic_write(file_name, temp_dir = Dir.tmpdir)
+      temp_file = Tempfile.new(File.basename(file_name), temp_dir)
+      yield temp_file
+      temp_file.close
+
+      begin
+        # Get original file permissions
+        old_stat = File.stat(file_name)
+      rescue Errno::ENOENT
+        # No old permissions, write a temp file to determine the defaults
+        check_name = File.join(File.dirname(file_name), ".permissions_check.#{Thread.current.object_id}.#{Process.pid}.#{rand(1000000)}")
+        File.open(check_name, "w") { }
+        old_stat = File.stat(check_name)
+        File.unlink(check_name)
+      end
+
+      # Overwrite original file with temp file
+      ::FileUtils.mv(temp_file.path, file_name)
+
+      # Set correct permissions on new file TODO: there is a secuity
+      # race here, where a sensitive tempfile could be momentarily world
+      # readable.
+      # TODO: don't do this unless root
+      File.chown(old_stat.uid, old_stat.gid, file_name)
+      File.chmod(old_stat.mode, file_name)
+    end
+  end
+
   def self.get_login_info(cache)
     username = File.read('/tmp/username.txt').strip rescue nil
     unless username
@@ -60,21 +78,19 @@ module GmailArchiver
         end
       end
 
-      log_ann('Connecting to imap.gmail.com')
+      $log.info('Connecting to imap.gmail.com')
       @imap = Net::IMAP.new('imap.gmail.com', 993, true)
-      log_ann('Authenticating')
+      $log.info('Authenticating')
       @imap.login(@username, @password)
     end
 
     def select
-      log_ann('Selecting all mail')
+      $log.info('Selecting all mail')
       @imap.select('[Gmail]/All Mail')
     end
   end
 
   class AbstractRunner
-    include StripeContext::Log::Loggable
-
     # Override
     def db_file
       raise NotImplementedError
@@ -91,17 +107,17 @@ module GmailArchiver
 
     def load_db
       if File.exists?(db_file)
-        log_ann("Loaded #{db_file}")
+        $log.info("Loaded #{db_file}")
         @db = YAML.load_file(db_file)
       else
-        log_ann("#{db_file} does not exist; initializing db")
+        $log.info("#{db_file} does not exist; initializing db")
         @db = {}
       end
     end
 
     def checkpoint_db
-      log_ann('Checkpointing db')
-      StripeContext::FileUtils.atomic_write(db_file) do |f|
+      $log.info('Checkpointing db')
+      Third.atomic_write(db_file) do |f|
         f.write(YAML.dump(@db))
       end
     end
@@ -125,7 +141,7 @@ module GmailArchiver
       records_per_day = records_per_second * 24 * 60 * 60
       estimate = (total - @db['consumed']) / records_per_day
 
-      log_ann("Just processed #{logical} logical records in #{sprintf('%.02f', delta)}s (actual: #{actual}). Total: #{@db['consumed']} in #{@db['elapsed']}s, for a total of #{sprintf('%.02f', records_per_second)} records per second, or #{records_per_day.to_i} records per day. I estimate #{sprintf('%.02f', estimate)} days remaining to process #{total} logical records.")
+      $log.info("Just processed #{logical} logical records in #{sprintf('%.02f', delta)}s (actual: #{actual}). Total: #{@db['consumed']} in #{@db['elapsed']}s, for a total of #{sprintf('%.02f', records_per_second)} records per second, or #{records_per_day.to_i} records per day. I estimate #{sprintf('%.02f', estimate)} days remaining to process #{total} logical records.")
     end
   end
 end
